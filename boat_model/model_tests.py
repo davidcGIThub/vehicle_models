b32afc2d8f2d83af8b807c6767d492472d5b4b9c
davidcGIThub <davidc@linuxpopper.org>
2023-05-22 09:57:53 -0600

    finished design boat model

--- boat_model/model_tests.py
+++ boat_model/model_tests.py
@@ -2,13 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(0,20,10000)
-c = 1
+c = 10
 # y = np.arctan((c+10)*(x))/(0.01+(x)) *(2/np.pi) * c
-y = np.arctan((c+10)*(x))/(0.01+(x)) *(2/np.pi) * c
+y = np.arctan((x**2))/(0.001 + x) *c
 
 f = 1/(x+1) * c
 
-z = 1/x * c
+z = 1/x * c * (np.pi/2)
 
 #
 
